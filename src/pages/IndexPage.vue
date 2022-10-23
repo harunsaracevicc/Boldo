80eba30dd769455c5da389a15deb89f106c4222a
Harun Saracevic <harunsaracevicc@gmail.com>
2022-10-23 17:27:14 +0200

    Making new section ( main-section )

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-header class="bg-primary text-white flex justify-center">
-    <div class="container q-pt-lg q-mb-lg">
-      <div class="navigation q-pb-xl flex justify-between items-center">
+  <q-header class="bg-primary text-white flex justify-center q-pb-md">
+    <div class="container q-pt-md q-mb-md">
+      <div class="navigation flex justify-between items-center">
         <div class="logo flex items-center">
           <img src="./../../public/images/logo/boldo-logo.png" alt="" />
           <span class="q-ml-sm">Boldo</span>
@@ -37,65 +37,181 @@
     </div>
   </q-header>
 
-  <q-page-container>
-    <div class="container flex row justify-between items-center" gap="93px">
-      <div class="left">
-        <div class="left-text">
-          <p class="primary-text text-white">
-            Save time by building <br />
-            fast with Boldo Template
-          </p>
-          <p class="secondary-text">
-            Funding handshake buyer business-to-business metrics iPad
-            partnership. <br />
-            First mover advantage innovator success deployment non-disclosure.
-          </p>
-        </div>
-        <div class="left-buttons q-mt-xl">
-          <q-btn
-            dense
-            text-color="primary"
-            align="center"
-            rounded
-            no-caps
-            color="secondary"
-            label="Buy template"
-            class="q-pl-xl q-pr-xl q-pt-md q-pb-md"
-          />
+  <section class="hero-section">
+    <q-header class="bg-primary text-white flex justify-center q-pb-md">
+      <div class="container q-pt-md q-mb-md">
+        <div class="navigation flex justify-between items-center">
+          <div class="logo flex items-center">
+            <img src="./../../public/images/logo/boldo-logo.png" alt="" />
+            <span class="q-ml-sm">Boldo</span>
+          </div>
 
-          <q-btn
-            dense
-            align="center"
-            outline
-            rounded
-            no-caps
-            color="white"
-            label="Explore"
-            class="q-ml-lg q-pl-xl q-pr-xl q-pt-md q-pb-md"
-          />
+          <div class="navigation-menu flex items-center">
+            <q-list class="flex q-ml-md q-mr-md">
+              <q-item clickable>
+                <q-item-section>Product</q-item-section>
+              </q-item>
+
+              <q-item clickable>
+                <q-item-section>Services </q-item-section>
+              </q-item>
+
+              <q-item clickable>
+                <q-item-section>About </q-item-section>
+              </q-item>
+            </q-list>
+
+            <q-btn
+              dense
+              flat
+              rounded
+              no-caps
+              class="nav-btn bg-white text-primary q-pr-xl q-pl-xl"
+              size="md"
+            >
+              Log in
+            </q-btn>
+          </div>
         </div>
       </div>
-      <div class="right">
-        <q-img
-          src="./../../public/images/logo/hero-graphics.png"
-          alt=""
-          width="480px"
-        />
-      </div>
-    </div>
+    </q-header>
+    <q-page>
+      <q-page-container>
+        <div class="container flex row justify-between items-center" gap="93px">
+          <div class="left">
+            <div class="left-text">
+              <p class="primary-text text-white">
+                Save time by building <br />
+                fast with Boldo Template
+              </p>
+              <p class="secondary-text">
+                Funding handshake buyer business-to-business metrics iPad
+                partnership. <br />
+                First mover advantage innovator success deployment
+                non-disclosure.
+              </p>
+            </div>
+            <div class="left-buttons q-mt-xl">
+              <q-btn
+                dense
+                text-color="primary"
+                align="center"
+                rounded
+                no-caps
+                color="secondary"
+                label="Buy template"
+                class="q-pl-xl q-pr-xl q-pt-md q-pb-md"
+              />
 
-    <div
-      class="sponsors container flex q-mt-xl q-pt-xl q-pb-xl justify-center items-center no-wrap"
-      width="100%"
-    >
-      <img src="./../../public/images/logo/Logo.png" href="" />
-      <img src="./../../public/images/logo/Presto-logo.png" />
-      <img src="./../../public/images/logo/Logo.png" href="" />
-      <img src="./../../public/images/logo/Presto-logo.png" />
-      <img src="./../../public/images/logo/Logo.png" href="" />
-      <img src="./../../public/images/logo/Presto-logo.png" />
-    </div>
-  </q-page-container>
+              <q-btn
+                dense
+                align="center"
+                outline
+                rounded
+                no-caps
+                color="white"
+                label="Explore"
+                class="q-ml-lg q-pl-xl q-pr-xl q-pt-md q-pb-md"
+              />
+            </div>
+          </div>
+          <div class="right">
+            <q-img
+              src="./../../public/images/logo/hero-graphics.png"
+              alt=""
+              width="480px"
+            />
+          </div>
+        </div>
+
+        <div
+          class="sponsors container flex q-mt-xl q-pt-xl q-pb-xl justify-center items-center no-wrap"
+          width="100%"
+        >
+          <img src="./../../public/images/logo/Logo.png" href="" />
+          <img src="./../../public/images/logo/Presto-logo.png" />
+          <img src="./../../public/images/logo/Logo.png" href="" />
+          <img src="./../../public/images/logo/Presto-logo.png" />
+          <img src="./../../public/images/logo/Logo.png" href="" />
+          <img src="./../../public/images/logo/Presto-logo.png" />
+        </div>
+      </q-page-container>
+    </q-page>
+  </section>
+
+  <section class="main-section">
+    <q-page class="bg-white">
+      <q-page-container>
+        <div class="container">
+          <div class="heading-text">
+            <div class="flex column justify-center text-center">
+              <p class="text-subtitle2 text-weight-light">Our Services</p>
+              <p class="text-h3 text-weight-light">
+                Handshake infographic mass market <br />
+                crowdfunding iteration.
+              </p>
+            </div>
+          </div>
+          <div class="feature-cards flex justify-between q-pt-xl q-ma-xl">
+            <q-card flat class="my-card">
+              <img src="./../../public/images/card-image1.png" />
+
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none text-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos qui fugiat.
+              </q-card-section>
+
+              <q-card-section>
+                <q-btn text-primary no-caps flat dense size="lg" href="/">
+                  Explore page ->
+                </q-btn>
+              </q-card-section>
+            </q-card>
+
+            <q-card flat class="my-card">
+              <img src="./../../public/images/card-image2.png" />
+
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none text-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos qui fugiat.
+              </q-card-section>
+              <q-card-section>
+                <q-btn text-primary no-caps flat dense size="lg" href="/">
+                  Explore page ->
+                </q-btn>
+              </q-card-section>
+            </q-card>
+            <q-card flat class="my-card">
+              <img src="./../../public/images/card-image3.png" />
+
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none text-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos qui fugiat.
+              </q-card-section>
+              <q-card-section>
+                <q-btn text-primary no-caps flat dense size="lg" href="/">
+                  Explore page ->
+                </q-btn>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </q-page-container>
+    </q-page>
+  </section>
+  <q-separator></q-separator>
 </template>
 
 <script>
@@ -103,10 +219,20 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+
+  setup() {},
 });
 </script>
 
 <style lang="scss">
+.q-card__section span.q-btn__content {
+  color: $primary;
+}
+.my-card {
+  max-width: 300px;
+  border-radius: 20px;
+}
+
 .container {
   margin-left: auto;
   margin-right: auto;
@@ -116,6 +242,7 @@ export default defineComponent({
 }
 .navigation {
   max-height: 42px !important;
+  z-index: 1;
 }
 
 .logo {
@@ -129,20 +256,22 @@ export default defineComponent({
 .nav-btn {
   height: 40px !important;
 }
-
-header::after {
-  z-index: -1;
-  content: "";
-  border-radius: 50%;
-  position: absolute;
-  width: 1293px;
-  height: 1293px;
-  left: 914px;
-  top: -789px;
-  background: #1c3d5b;
+.q-header {
+  z-index: 9999 !important;
 }
 
-.q-page-container {
+// .q-header::after {
+//   content: "";
+//   border-radius: 50%;
+//   position: absolute;
+//   width: 1293px;
+//   height: 1293px;
+//   left: 862px;
+//   top: -789px;
+//   background: #1c3d5b49;
+// }
+
+.hero-section .q-page-container {
   background: $primary;
 }
 
@@ -151,7 +280,6 @@ header::after {
 }
 .right {
   max-width: 500px;
-  z-index: 2001;
 }
 .primary-text {
   font-style: normal;
@@ -184,5 +312,8 @@ header::after {
     black 50%,
     transparent 100%
   );
+}
+p.text-h3 {
+  line-height: 4.5rem;
 }
 </style>
